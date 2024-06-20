afb1ab4d05f937c9422ab4e38284104e55f6c071
edenjenzohar <edenzohar04@gmail.com>
2024-06-20 11:15:49 +0300

    Initial script commit

---
+++ scripts/encodings_retrival/extract_cellPLM_encodings.py
@@ -0,0 +1,5 @@
+# Script to extract the encodings for all genes from a cellPLM model and save them to a csv file.
+# Paper can be found at https://www.biorxiv.org/content/10.1101/2023.10.03.560734v1.full
+# Github https://github.com/OmicsML/CellPLM
+# Directs us to the dropBox folder for downloading the model check-point:
+# https://www.dropbox.com/scl/fo/i5rmxgtqzg7iykt2e9uqm/h?rlkey=o8hi0xads9ol07o48jdityzv1&e=1&dl=0
