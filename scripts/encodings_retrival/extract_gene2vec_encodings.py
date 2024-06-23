46489acc220b30e692a260e9f37a44a869998d01
edenjenzohar <edenzohar04@gmail.com>
2024-06-23 10:46:21 +0300

    Add save encodings method

--- scripts/encodings_retrival/extract_gene2vec_encodings.py
+++ scripts/encodings_retrival/extract_gene2vec_encodings.py
@@ -1,19 +1,38 @@
 # Script to extract the encodings for all genes from gene2vec and save them to a csv file.
 # The encodings are taken from https://github.com/jingcheng-du/Gene2vec/tree/master/pre_trained_emb
-import tempfile
 from pathlib import Path
 
 import click
+import pandas as pd
 
 GENE2VEC_URL = "https://github.com/jingcheng-du/Gene2vec/blob/6236e0b21fbc367bf8ff5695ed0cc1443861bd1e/pre_trained_emb/gene2vec_dim_200_iter_9_w2v.txt"
 
 
-def load_gene2vec_encodings():
-    pass
+def load_encodings_w2v_format(path_to_data):
+    return pd.read_csv(
+        path_to_data,
+        sep=" ",
+        index_col=0,
+        header=None,
+        skiprows=1,
+    )
 
 
-def save_encodings():
-    pass
+def save_encodings(encodings, output_file_dir):
+    """
+    Save the gene encodings to the output dir.
+
+    Args:
+    ----
+        encodings (pd.DataFrame): the encodings
+        model_type (str): "blood" or "human"
+        output_file_dir (str): path to dir for saving the file
+
+    """
+    model_encodings_dir = Path(output_file_dir) / "Gene2Vec"
+    model_encodings_dir.mkdir(parents=True, exist_ok=True)
+    output_file_path = model_encodings_dir / "encodings.csv"
+    encodings.to_csv(output_file_path, index_label="symbol")
 
 
 @click.command()
@@ -27,7 +46,7 @@ def save_encodings():
 @click.option(
     "--input-file-dir",
     type=click.STRING,
-    help="The path to the directory with the data files",
+    help="The path to the csv file with the encodings",
     default=None,
 )
 @click.option(
@@ -36,11 +55,10 @@ def save_encodings():
     help="output files path",
 )
 def main(allow_downloads, input_file_dir, output_file_dir):
+
     if allow_downloads:
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            tmpdir = Path(tmpdirname)
-            encodings = load_gene2vec_encodings(model_dir=tmpdir)
+        encodings = load_encodings_w2v_format(GENE2VEC_URL)
     else:
-        encodings = load_gene2vec_encodings(model_dir=input_file_dir)
+        encodings = load_encodings_w2v_format(input_file_dir)
 
     save_encodings(encodings, output_file_dir)
