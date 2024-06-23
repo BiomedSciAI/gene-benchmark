2b37453b710bd164560b3a14eac2e30b6a506a27
edenjenzohar <edenzohar04@gmail.com>
2024-06-23 15:01:33 +0300

    Add method for appending the symbols as the index

--- scripts/encodings_retrival/extract_geneformer_encodings.py
+++ scripts/encodings_retrival/extract_geneformer_encodings.py
@@ -1,14 +1,44 @@
+import pickle
 import tempfile
 from pathlib import Path
 
 import click
+import pandas as pd
+import requests
 import torch
 
 GENEFORMER_URL = "https://huggingface.co/ctheodoris/Geneformer/resolve/main/pytorch_model.bin?download=true"
+TOKEN_DICT_URL = "https://huggingface.co/ctheodoris/Geneformer/resolve/main/geneformer/token_dictionary.pkl?download=true"
 
 
 def download_geneformer(GENEFORMER_URL):
-    pass
+    local_filename = "pytorch_model.bin"
+    with requests.get(GENEFORMER_URL, stream=True) as response:
+        response.raise_for_status()
+        with open(local_filename, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+
+def download_token_dict(TOKEN_DICT_URL):
+    local_filename = "token_dictionary.pkl"
+    with requests.get(TOKEN_DICT_URL, stream=True) as response:
+        response.raise_for_status()
+        with open(local_filename, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+
+def add_tokens_as_index(encodings, token_dict):
+    encodings_with_symbols = pd.DataFrame(
+        encodings["bert.embeddings.word_embeddings.weight"].detach(),
+        index=list(token_dict.keys()),
+    )
+    encodings_with_symbols = encodings_with_symbols.reset_index()
+    encodings_with_symbols = encodings_with_symbols.rename(
+        columns={"index": "ensembl_gene_id"}
+    )
+    return encodings_with_symbols
 
 
 def save_encodings():
@@ -41,11 +71,20 @@ def main(allow_downloads, input_file_dir, output_file_dir):
         with tempfile.TemporaryDirectory() as tmpdirname:
             tmpdir = Path(tmpdirname)
             download_geneformer(tmpdir)
-            encodings = torch.load(tmpdir, map_location=torch.device("cpu"))
+            download_token_dict(tmpdir)
+            path_to_data = Path(tmpdir) / "pytorch_model.bin"
+            path_to_tokens = Path(tmpdir) / "token_dictionary.pkl"
+            with open(path_to_tokens, "rb") as file:
+                token_dict = pickle.load(file)
+            encodings = torch.load(path_to_data, map_location=torch.device("cpu"))
     else:
         path_to_data = Path(input_file_dir) / "pytorch_model.bin"
         encodings = torch.load(path_to_data, map_location=torch.device("cpu"))
+        path_to_tokens = Path(input_file_dir) / "token_dictionary.pkl"
+        with open(path_to_tokens, "rb") as file:
+            token_dict = pickle.load(file)
 
+    encodings = add_tokens_as_index(encodings, token_dict)
     save_encodings(encodings, output_file_dir)
 
 
