091ffe1ba6413e756bc3bbd8e562dc611d583731
Yoav Kan-Tor <Yoav.Kan-Tor@ibm.com>
2024-06-24 14:39:20 +0300

    Squashed commit of the following:
    commit ccb0cda924609083e852ae77ebac2e8fe87762a9
    Merge: 4b1e3d1 715e118
    Author: YoavKT <yoav.kan-tor@ibm.com>
    Date:   Mon Jun 24 14:37:05 2024 +0300
    
        Merge pull request #14 from BiomedSciAI/genefactor_using_utils
    
        Geneformer using utils
    
    commit 715e118d19f2bff4651afac80109ebbc3a807246
    Author: Yoav Kan-Tor <Yoav.Kan-Tor@ibm.com>
    Date:   Mon Jun 24 12:58:45 2024 +0300
    
        fix the local load
    
    commit 4b1e3d143e733eede6ef5043ffaa333eaca6cf5c
    Merge: 9c44a64 ab11d25
    Author: edenjenzohar <80422907+edenjenzohar@users.noreply.github.com>
    Date:   Mon Jun 24 12:03:37 2024 +0300
    
        Merge pull request #15 from BiomedSciAI/create_geneformer_extraction_script
    
        Create geneformer extraction script
    
    commit 9c44a647ea960c2e8003d3315ea767385c5cb3c7
    Merge: b893500 98a597a
    Author: YoavKT <yoav.kan-tor@ibm.com>
    Date:   Mon Jun 24 11:58:19 2024 +0300
    
        Merge pull request #13 from BiomedSciAI/gene2vec_utils
    
        Gene2vec using utils
    
    commit ab11d25bc786e3b409a01a68113711e90860bc00
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Mon Jun 24 11:48:56 2024 +0300
    
        add args to save encodings method
    
    commit 2c404364a39dc7fd072d235857e71b26564f68ba
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Mon Jun 24 11:12:15 2024 +0300
    
        Add documentation
    
    commit 8c5e6798c14e647883b1255ddbe4e088154adb75
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Mon Jun 24 10:51:55 2024 +0300
    
        Add return of downloaded file path
    
    commit 473d3c24749d22cfca85e7a21be06e5805aab97f
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Mon Jun 24 10:42:17 2024 +0300
    
        Remove repetitive method
    
    commit 369d3eabc8216a4be66a8d76b19a4c1baaa53610
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Sun Jun 23 18:31:42 2024 +0300
    
        Add symbol as index
    
    commit 2db478b0f4c977a5749c45fbc304222512072560
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Sun Jun 23 18:25:33 2024 +0300
    
        Update dir path in tempdir
    
    commit 70d2bf2dfa2402b626b4778e496ab70cdbe094f1
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Sun Jun 23 16:35:12 2024 +0300
    
        add saving encodings method
    
    commit 2b37453b710bd164560b3a14eac2e30b6a506a27
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Sun Jun 23 15:01:33 2024 +0300
    
        Add method for appending the symbols as the index
    
    commit ce829da81b300937731f1d86c410474e0859a098
    Author: edenjenzohar <edenzohar04@gmail.com>
    Date:   Sun Jun 23 14:34:42 2024 +0300
    
        Initial commit

---
+++ scripts/encodings_retrival/extract_geneformer_encodings.py
@@ -0,0 +1,126 @@
+import pickle
+import tempfile
+from pathlib import Path
+
+import click
+import pandas as pd
+import requests
+import torch
+
+GENEFORMER_URL = "https://huggingface.co/ctheodoris/Geneformer/resolve/main/pytorch_model.bin?download=true"
+TOKEN_DICT_URL = "https://huggingface.co/ctheodoris/Geneformer/resolve/main/geneformer/token_dictionary.pkl?download=true"
+
+
+def download_file_from_url(url, file_name, dir_path, chunk_size=8192):
+    """
+    Download a file fom a url path.
+
+    Args:
+    ----
+        url (str): url of target file
+        file_name (str): name of output file
+        dir_path (pathlib.Path): path to dir for saving the file
+        chunk_size (int): size of data to be streamed
+
+    Returns:
+    -------
+        The path of the saved file
+
+    """
+    local_filename = dir_path / file_name
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        with open(local_filename, "wb") as file:
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                file.write(chunk)
+    return local_filename
+
+
+def add_tokens_as_index(encodings, token_dict):
+    """
+    Add tokens names to the encodings file.
+
+    Args:
+    ----
+        encodings (torch.tensor): the encodings
+        token_dict (dict): mapping of symbol name to index
+
+    Returns:
+    -------
+        encodings with the symbols
+
+    """
+    encodings_with_symbols = pd.DataFrame(
+        encodings["bert.embeddings.word_embeddings.weight"].detach(),
+        index=list(token_dict.keys()),
+    )
+    return encodings_with_symbols
+
+
+def save_encodings(
+    encodings, output_file_dir, sub_dir_name="Geneformer", file_name="encodings.csv"
+):
+    """
+    Save the gene encodings to the output dir.
+
+    Args:
+    ----
+        encodings (pd.DataFrame): the encodings
+        output_file_dir (str): path to dir for saving the file
+        sub_dir_name (str): name od sub directory to save the encodings.
+        file_name (str): name of encoding file
+
+    """
+    model_encodings_dir = Path(output_file_dir) / sub_dir_name
+    model_encodings_dir.mkdir(parents=True, exist_ok=True)
+    output_file_path = model_encodings_dir / file_name
+    encodings.to_csv(output_file_path, index_label="symbol")
+
+
+@click.command()
+@click.option(
+    "--allow-downloads",
+    "-l",
+    type=click.BOOL,
+    help=f"download files directly from {GENEFORMER_URL}, use this option only if you trust the URL.",
+    default=False,
+)
+@click.option(
+    "--input-file-dir",
+    type=click.STRING,
+    help="The path to the csv file with the encodings",
+    default=None,
+)
+@click.option(
+    "--output-file-dir",
+    type=click.STRING,
+    help="output files path",
+    default="./encodings",
+)
+def main(allow_downloads, input_file_dir, output_file_dir):
+
+    if allow_downloads:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            tmpdir = Path(tmpdirname)
+            path_to_data = download_file_from_url(
+                GENEFORMER_URL, file_name="pytorch_model.bin", dir_path=tmpdir
+            )
+            path_to_tokens = download_file_from_url(
+                TOKEN_DICT_URL, file_name="token_dictionary.pkl", dir_path=tmpdir
+            )
+            with open(path_to_tokens, "rb") as file:
+                token_dict = pickle.load(file)
+            encodings = torch.load(path_to_data, map_location=torch.device("cpu"))
+    else:
+        path_to_data = Path(input_file_dir) / "pytorch_model.bin"
+        encodings = torch.load(path_to_data, map_location=torch.device("cpu"))
+        path_to_tokens = Path(input_file_dir) / "token_dictionary.pkl"
+        with open(path_to_tokens, "rb") as file:
+            token_dict = pickle.load(file)
+
+    encodings = add_tokens_as_index(encodings, token_dict)
+    save_encodings(encodings, output_file_dir)
+
+
+if __name__ == "__main__":
+    main()
