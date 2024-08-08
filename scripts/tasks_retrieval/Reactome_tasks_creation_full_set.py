a32e2a393dec8e7b609e061631ae00190fbaa79c
Yoav Kan-Tor <Yoav.Kan-Tor@ibm.com>
2024-08-08 14:36:39 +0300

    Fill documentation

--- scripts/tasks_retrieval/Reactome_tasks_creation_full_set.py
+++ scripts/tasks_retrieval/Reactome_tasks_creation_full_set.py
@@ -20,58 +20,100 @@ BOTTOM_PATHWAY_TO_GENE_LIST = (
 PATHWAY_DES = "https://reactome.org/download/current/ReactomePathways.txt"
 
 
-def get_pathway_to_genes_dict(url):
-    gene_list_file = pd.read_csv(url, on_bad_lines="skip", header=None, delimiter="\t")
-    pathway_to_gene_df = pd.DataFrame(
-        columns=["pathway_description", "pathway", "genes"]
-    )
-    pathway_to_gene_df[["pathway_description", "pathway"]] = gene_list_file.iloc[:, :2]
-    pathway_to_gene_df["genes"] = gene_list_file.iloc[:, 2:].apply(
-        lambda x: [str(v) for v in set(x) if not pd.isna(v)], axis=1
-    )
-    pathway_to_gene_df = pathway_to_gene_df.set_index("pathway")
-    return pathway_to_gene_df["genes"].to_dict()
-
-
-def read_gmt_from_url(url, gmt_filename):
+def get_pathway_symbols_dict(
+    url: str, gmt_filename: str, allow_downloads: bool
+) -> dict[str, list[str]]:
     """
     Downloads a ZIP file from the given URL, extracts the specified GMT file,
     and returns its contents as a dictionary.
 
     Args:
     ----
-        url (str): URL to the ZIP file.
+        url (str): URL of path for a zip file
         gmt_filename (str): The name of the GMT file within the ZIP archive.
+        allow_downloads (bool): indicator if the file is a url or not
+
+    Raises:
+    ------
+        Exception: _description_
 
     Returns:
     -------
-        dict: A dictionary where keys are gene set names and values are lists of genes.
+        dict[str,list[str]]: A dictionary where keys are pathway names and values are lists of genes.
 
     """
-    # Step 1: Download the ZIP file
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(f"Failed to download ZIP file: {response.status_code}")
+    if allow_downloads:
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception(f"Failed to download ZIP file: {response.status_code}")
+        fstream = io.BytesIO(response.content)
+    else:
+        fstream = url
+    with zipfile.ZipFile(fstream) as zip_ref:
+        gmt_content = get_gmt_from_zip(zip_ref, gmt_filename)
+    return gmt_file_to_dictionary(gmt_content)
 
-    # Step 2: Extract the GMT file from the ZIP archive
-    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
-        if gmt_filename not in zip_ref.namelist():
-            raise Exception(f"{gmt_filename} not found in the ZIP archive")
-        with zip_ref.open(gmt_filename) as gmt_file:
-            gmt_content = gmt_file.read().decode("utf-8")
 
-    # Step 3: Parse the GMT file content
+def get_gmt_from_zip(zip_ref, gmt_filename: str):
+    """
+    extract the GMT file from a zip archive.
+
+    Args:
+    ----
+        zip_ref (zipfile.ZipFile): Zip archive containing a gmt file
+        gmt_filename (str): The archived GMT file name
+
+    Raises:
+    ------
+        Exception: raised if the zip archive doesn't contain the file
+
+    Returns:
+    -------
+        str: the gmt_file file content
+
+    """
+    if gmt_filename not in zip_ref.namelist():
+        raise Exception(f"{gmt_filename} not found in the ZIP archive")
+    with zip_ref.open(gmt_filename) as gmt_file:
+        gmt_content = gmt_file.read().decode("utf-8")
+    return gmt_content
+
+
+def gmt_file_to_dictionary(gmt_content: str) -> dict[str, list[str]]:
+    r"""
+    Turns a reactom gmt file into a dictionary.
+
+    Args:
+    ----
+        gmt_content (str): A file where each line is separated by \n and values by \t
+
+    Returns:
+    -------
+        dict[str,list[str]]: A dictionary where keys are pathway names and values are lists of genes.
+
+    """
     gmt_dict = {}
     for line in gmt_content.strip().split("\n"):
         parts = line.strip().split("\t")
         description = parts[1]
         genes = parts[2:]
         gmt_dict[description] = genes
-
     return gmt_dict
 
 
-def get_hierarchy_data(url):
+def get_hierarchy_data(url: str) -> [dict[str, str], pd.DataFrame]:
+    """
+    loads and returns the pathways hierarchies.
+
+    Args:
+    ----
+        url (str): the location of the file
+
+    Returns:
+    -------
+        [dict[str,str],pd.DataFrame]: a tuple with dictionary with parent pathways as keys and children as values and in a data frame format
+
+    """
     hierarchy_df = pd.read_csv(
         url, header=None, delimiter="\t", names=["parent", "child"]
     )
@@ -83,7 +125,20 @@ def get_hierarchy_data(url):
     return hierarchies, hierarchy_df
 
 
-def pathways_2_one_hot(pathways, path_2_gene):
+def pathways_2_one_hot(pathways: list[str], path_2_gene) -> pd.DataFrame:
+    """
+    create a one hot data frame with the genes in the rows and pathways as columns.
+
+    Args:
+    ----
+        pathways (list[str]): A list of pathways to analyze
+        path_2_gene (PathwaySeeks): PathwaySeeks object with the corresponding gene to list of genes
+
+    Returns:
+    -------
+        pd.DataFrame: a one hot data frame
+
+    """
     task_df = pd.DataFrame(columns=["genes"], index=pathways)
     task_df["genes"] = [";".join(path_2_gene.get_genes(path)) for path in task_df.index]
     return list_form_to_onehot_form(
@@ -92,8 +147,25 @@ def pathways_2_one_hot(pathways, path_2_gene):
 
 
 def pathway_set_to_task(
-    pathways, path_2_gene, pathway_des, pathway_identifier, main_task_directory, verbose
+    pathways: list[str],
+    path_2_gene,
+    pathway_des: pd.DataFrame,
+    pathway_identifier: str,
+    main_task_directory: str,
+    verbose: bool,
 ):
+    """
+    create a task from a set of pathways.
+
+    Args:
+    ----
+        pathways (list[str]): the list of pathways to be collected into the task
+        path_2_gene (PathwaySeeks): PathwaySeeks object with the corresponding gene to list of genes
+        pathway_des (pd.DataFrame): Dataframe with pathways identifier as index and a name column
+        pathway_identifier (str): the pathways identifier
+        verbose (bool): if true will do some prints
+
+    """
     outcomes = pathways_2_one_hot(pathways, path_2_gene)
     outcomes.rename(pathway_des["name"].to_dict())
     symbols = pd.Series(outcomes.index, name="symbol")
@@ -104,7 +176,19 @@ def pathway_set_to_task(
         )
 
 
-def get_gene_descriptions(url):
+def get_gene_descriptions(url: str) -> pd.DataFrame:
+    """
+    retrieves the data frame description.
+
+    Args:
+    ----
+        url (str): the file location
+
+    Returns:
+    -------
+        pd.DataFrame: data frame with pathways descriptions
+
+    """
     return pd.read_csv(
         url,
         on_bad_lines="skip",
@@ -115,7 +199,22 @@ def get_gene_descriptions(url):
     )
 
 
-def get_top_level_dict(hierarchy_df, pathway_des):
+def get_top_level_dict(
+    hierarchy_df: pd.DataFrame, pathway_des: pd.DataFrame
+) -> dict[str, list[str]]:
+    """
+    returns a dictionary with the top pathways of each species.
+
+    Args:
+    ----
+        hierarchy_df (pd.DataFrame): A data frame containing the hierarchy of pathways
+        pathway_des (pd.DataFrame): A data frame containing the
+
+    Returns:
+    -------
+        dict[str,list[str]]: _description_
+
+    """
     child_set = set(hierarchy_df["child"].values)
     top_level = list(filter(lambda x: not x in child_set, hierarchy_df.index))
     pathway_des["top_level"] = [path in top_level for path in pathway_des.index]
@@ -130,10 +229,28 @@ def get_top_level_dict(hierarchy_df, pathway_des):
 
 @dataclass
 class PathwaySeeks:
+    """
+    The data from reactom contains the gene list for bottom level pathways only to prevent redundant calculations
+    this class saves the gene list of each pathway when it encounter its.
+    """
+
     pathway_to_gene: dict[str, list[str]]
     hierarchies: dict[str, list[str]]
 
     def get_genes(self, pathway: list[str], verbose=False) -> list[str]:
+        """
+        retrives the gene list of a pathway by merging all of it's descendants genes.
+
+        Args:
+        ----
+            pathway (list[str]): A list of pathways
+            verbose (bool, optional): If true warnings will be printed. Defaults to False.
+
+        Returns:
+        -------
+            list[str]: a list of gene symbols
+
+        """
         sub_genes = set()
         if pathway in self.pathway_to_gene:
             return self.pathway_to_gene[pathway]
@@ -228,7 +345,7 @@ def main(
     pathway_des = get_gene_descriptions(pathway_des_url)
     hire_dict, hierarchy_df = get_hierarchy_data(hierarchies_file_url)
     path_2_gene = PathwaySeeks(
-        read_gmt_from_url(pathway_to_gene_url, "ReactomePathways.gmt"), hire_dict
+        get_pathway_symbols_dict(pathway_to_gene_url, "ReactomePathways.gmt"), hire_dict
     )
     if add_top_levels:
         top_dict = get_top_level_dict(hierarchy_df, pathway_des)
